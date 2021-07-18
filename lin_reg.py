56cbb335981d5482b3ddb9c4b71c9045e536b1ce
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-07-18 14:45:08 +0100

    Improving code

--- lin_reg.py
+++ lin_reg.py
@@ -2,11 +2,10 @@
 from sklearn import model_selection
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
-# from mini_batch import MiniBatch
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
+# split dataset into train and evaluation by 80 / 20
 X, y = load_boston(return_X_y=True)
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.8)
 X_train, X_val, y_train, y_val = model_selection.train_test_split(X, y, test_size=0.8)
@@ -26,8 +25,9 @@ class DataLoader:
             idx += batch_size
 
     def __getitem__(self, idx):
-        return self.batches[idx]  # X, y
+        return self.batches[idx]  # X, y (in this case, 16 vectors)
 
+train_loader = DataLoader(X, y)
 
 #%%
 class LinearRegression:
@@ -38,33 +38,33 @@ class LinearRegression:
     def _get_mean_squared_error_loss(self, y_hat, y):
         return np.mean((y_hat - y) ** 2)
     
-    def fit(self, X, y, epochs=1000):
+    def fit(self, X, y, epochs=50):
         # controls scaling of step-size
-        lr = 0.0000000005
+        lr = 0.000001
         # initialise list of all costs
         all_costs = []
         for epoch in range(epochs):
-            # make predictions (y_hat)
-            predicted_values = self.predict(X)
-            # compute loss and print
-            loss = self._get_mean_squared_error_loss(predicted_values, y)
-            # compute the gradient of the loss with respect to model params
-            grad_w, grad_b = self._compute_grads(X, y)
-            # update the params 
-            self.w -= lr * grad_w # update weight
-            self.b -= lr * grad_b # update bias
-            # calculate mean squared error
-            all_costs.append(loss)
+            for X, y in train_loader.batches:
+                # make predictions (y_hat)
+                y_hat = self.predict(X)
+                # compute loss and print
+                loss = self._get_mean_squared_error_loss(y_hat, y)
+                # compute the gradient of the loss with respect to model params
+                grad_w, grad_b = self._compute_grads(X, y) # compute gradient for each weight and bias
+                # update the params 
+                self.w -= lr * grad_w # update weight
+                self.b -= lr * grad_b # update bias
+                all_costs.append(loss)
+                print('loss:', loss)
         plot_loss(all_costs)
     
     def predict(self, X):
         return np.matmul(X, self.w) + self.b
 
     def _compute_grads(self, X, y):
+        grads_for_individual_examples = []
         y_hat = self.predict(X)
         grad_b = 2 * np.mean(y_hat - y)
-
-        grads_for_individual_examples = []
         for i in range(len(X)):
             grad_i = 2 * y_hat[i] - y[i] * X[i] # rate of change of loss with respect to weights for a particular example
             grads_for_individual_examples.append(grad_i)
@@ -72,12 +72,9 @@ class LinearRegression:
         # the grad_w is what we will subtract from each of the Ws
         # each W is going to affect the predictions differently, which means they're going to affect the loss differently.
         return grad_w, grad_b
-
-    
+  
     def scalar_l1_norm(self):
         return np.linalg.norm(self.w, ord=1)
-
-    # def score(self):
 
 #%%
 def plot_predictions(y_pred, y_true):
@@ -102,29 +99,7 @@ def plot_variance_bias(bias, variance):
     plt.figure()
     plt.ylabel()
 
-def run_model_on_data(model):
-    # 	- fit a linear regression model on the training set
-    params = model.fit(X_train, y_train)
-
-    # 	- score your model on the training set
-    training_score = model.score(X_train, y_train)
-    #         print(f"training_score: {training_score}")
-
-    # 	- score your model on the test set
-    testing_score = model.score(X_test, y_test)
-    #         print(f"testing_score: {testing_score}")
-
-    training_score = model.score(X_train, y_train)
-    #         print(f"training_score: {training_score}")
-
-    validation_score = model.score(X_val, y_val)
-    #         print(f"validation_score: {validation_score}")
-
-    return training_score, testing_score, validation_score
 #%%
 linear_model = LinearRegression(n_features=X.shape[1]) # make predictions on data
 linear_model.fit(X, y)
 pred = linear_model.predict(X)
-# run_model_on_data(linear_model)
-
-# %%
